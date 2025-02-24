8340f122816a77bcd62c1d76960c83c6a1cb1f84
NicoSan13 <nalavagiler@gmail.com>
2025-02-23 23:05:24 -0500

    Agregar pruebas para la API de indicadores

--- test_app.py
+++ test_app.py
@@ -6,4 +6,11 @@ def test_index():
     cliente = app.app.test_client()
     respuesta = cliente.get("/")
     assert respuesta.status_code == 200
-    assert b"Indicadores del curso" in respuesta.data
+
+
+def test_api():
+    app.app.testing = True
+    cliente = app.app.test_client()
+    respuesta = cliente.get("/api/indicadores")
+    assert respuesta.status_code == 200
+    assert respuesta.content_type == "application/json"
